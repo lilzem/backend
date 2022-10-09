1367ca5086f453d7cf4b5f208ecd78e977425d37
lilzem <dmytrynskiy74@gmail.com>
2022-10-09 20:26:14 +0300

    added models

---
+++ app/Models/CostRecord.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CostRecord extends Model
+{
+    use HasFactory;
+}
